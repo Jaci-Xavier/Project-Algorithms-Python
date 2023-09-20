61536d4b9f48eccec3e68c518fb9d32f414cb4bc
Jaci-Xavier <devxvr@gmail.com>
2023-09-20 18:56:14 -0300

    req 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message('vacilo', 'vacilo')
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(80, 80)
+    assert encrypt_message('vacilo', 80) == 'olicav'
+    assert encrypt_message('vacilo', 1) == 'v_olica'
