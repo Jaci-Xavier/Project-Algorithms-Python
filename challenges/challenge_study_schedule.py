cf5fef9f1640f1fe2091e7b13276eb2f57a56d75
Jaci-Xavier <devxvr@gmail.com>
2023-09-20 17:16:42 -0300

    req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+
+    try:
+        for start_time, end_time in permanence_period:
+            if start_time <= target_time <= end_time:
+                counter += 1
+        return counter
+    except (TypeError):
+        return None
