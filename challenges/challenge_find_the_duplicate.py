bfeeca23f6e30e6f8d275c663ff6941454ebad3c
Jaci-Xavier <devxvr@gmail.com>
2023-09-22 22:34:15 -0300

    req 05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,17 @@
+def remove_negative_numbers(list):
+    return [num for num in list if isinstance(num, int) and num > 0]
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(nums, list) or len(nums) < 2:
+        return False
+
+    nums = remove_negative_numbers(nums)
+    counter = set()
+
+    for num in nums:
+        if num in counter:
+            return num
+        counter.add(num)
+
+    return False
